da4a997f39bf1434fcfa4bafc97dc11fda93ca52
Abhishek Mane <abhishekmane@outlook.in>
2020-01-04 00:16:15 +0530

    feat: added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+# Author    : Abhishek Mane <abhishekmane@outlook.in>
+# Date      : 03/01/2020
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    version='1',
+    name='registry-update-job',
+    packages=find_packages(),
+    include_package_data=True
+)
